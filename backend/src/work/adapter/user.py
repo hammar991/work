455a6e49a7da7ab40bd1bbd6dd7672375bf95b69
hammar <yujiale20020131@163.com>
2026-02-27 19:27:01 +0800

    Reapply "oidc 鉴权"
    This reverts commit ee588f40d0f2a102bf055e79f2f045719aeac264.

---
+++ backend/src/work/adapter/user.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from sqlmodel import Session, select
+from work.schemas.entity import User
+
+
+class UserAdapter:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_by_oidc_id(self, oidc_id: str) -> Optional[User]:
+        statement = select(User).where(User.oidc_id == oidc_id)
+        return self.session.exec(statement).first()
+
+    def create(self, user: User) -> User:
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        return user
+
+    def update(self, user: User) -> User:
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        return user
+
+    def get_by_id(self, serial: int) -> Optional[User]:
+        statement = select(User).where(User.serial == serial)
+        return self.session.exec(statement).first()
+
+    def get_by_unique_id(self, unique_id: str) -> Optional[User]:
+        statement = select(User).where(User.unique_id == unique_id)
+        return self.session.exec(statement).first()
