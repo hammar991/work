03cf1f4dc92413c92df026851e71604a224dcfe5
a <local@local.dev>
2026-02-28 12:26:00 +0800

    环境变量

--- backend/src/work/core/settings.py
+++ backend/src/work/core/settings.py
@@ -1,17 +1,21 @@
 from pydantic_settings import BaseSettings
 from pydantic import AnyUrl, ValidationError
 from loguru import logger
+from pathlib import Path
 
+
+logger.debug(list(Path().glob("*")))
 
 class Settings(BaseSettings):
     db_url: AnyUrl
 
     # OIDC 服务商配置
-    OIDC_NAME : str = ""
-    OIDC_CLIENT_ID = ""
-    OIDC_CLIENT_SECRET = ""
+    # noinspection SpellCheckingInspection
+    OIDC_NAME : str = "Authentik"
+    OIDC_CLIENT_ID: str
+    OIDC_CLIENT_SECRET: str
     # 发现地址
-    OIDC_DISCOVERY_URL = ""
+    OIDC_DISCOVERY_URL: str
     # 回调地址，需要在 OIDC 控制台配置白名单
     REDIRECT_URI = "http://localhost:8000/auth/callback"
     OIDC_SCOPE = ["openid", "email", "profile"]  # 需要改
@@ -24,7 +28,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 720
 
     class Config:
-        env_file = "../.env"
+        env_file = ".env"
 
 try:
     # noinspection PyArgumentList
