e57f880d626729ab7b045bfb7b6177651a42afd0
hammar <yujiale20020131@163.com>
2026-02-28 11:58:52 +0800

    sync with latest upstream

--- backend/src/work/utils/utils.py
+++ backend/src/work/utils/utils.py
@@ -1,9 +1,15 @@
+import nanoid
 from uuid import uuid4
-import secrets
+
+
+def gen_nuid() -> str:
+    return nanoid.generate()
 
 
 def gen_uuid() -> str:
     return str(uuid4())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    print(gen_nuid())
     print(gen_uuid())
