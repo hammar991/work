af837fdb72614d754d07f208e1d2356fa00c6fce
hammar <yujiale20020131@163.com>
2026-03-05 13:52:10 +0800

    前端 点击跳转对应项目详情页

--- frontend/src/views/ListProjectView.vue
+++ frontend/src/views/ListProjectView.vue
@@ -7,7 +7,10 @@
     <div v-for="item, index in projectList" :key="index">
       <n-card>
         <template #header>
-          <n-text type="primary">{{ item.title }}</n-text>
+          <n-text
+            type="primary"
+            @click="goToDetail(item.title)"
+            >{{ item.title }}</n-text>
         </template>
         <n-text>{{ item.content }}</n-text>
       </n-card>
@@ -19,7 +22,9 @@
 import { ref } from 'vue'
 import { useClient } from '@/client/client'
 import type { ProjectEntity } from '@/types/dto'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 const client = useClient()
 const projectList = ref<Array<ProjectEntity>>([])
 
@@ -28,4 +33,10 @@ const projectList = ref<Array<ProjectEntity>>([])
 client.listProject().then(data => {
   projectList.value = data
 })
+
+
+const goToDetail = (keyword: string) => {
+  console.log(keyword)
+  router.push({ name: '项目详情', params: { keyword } })
+}
 </script>
